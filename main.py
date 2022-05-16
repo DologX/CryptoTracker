21425ef749307f2cac84e9a2c40bcc3c773c3b94
dologx <turcas.david.emil201@gmail.com>
2022-05-16 23:52:48 +0300

    Version 1.0

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 import requests
 import time
 from picamera import PiCamera
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 
 
 # Get eGLD Price
@@ -41,14 +44,24 @@ def display_menu():
     print("Type '2' to show MEX price")
     print("Type '3' to show RIDE price")
     print("Type '4' to show ITHEUM price")
+    print("Type '5' to show last price reaction")
     print()
     print("Type '0' to exit")
     print()
 
 
+def show_reaction(message):
+    image = Image.open("/home/pi/Desktop/CryptoTracker/reaction.jpg")
+    image_draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype("FreeMono.ttf", 200)
+    image_draw.text((70, 70), message, font=font, fill=(255, 0, 0))
+    image.show()
+
+
 def main():
     camera = PiCamera()
     time.sleep(2)
+    last_price = ""
 
     while True:
         display_menu()
@@ -57,17 +70,31 @@ def main():
         if command == '0':
             exit()
         elif command == '1':
-            print("eGLD Value: " + str(get_egld_price())[:8] + "$\n\n")
+            price = str(get_egld_price())[:8]
+            print("eGLD Value: " + price + "$\n\n")
+            last_price = "eGLD = " + price + "$"
+            print("Input your face reaction in 2 seconds!\n")
             take_photo(camera)
         elif command == '2':
-            print("MEX Value: " + str(get_mex_price())[:8] + "$\n\n")
+            price = str(get_mex_price())[:8]
+            print("MEX Value: " + price + "$\n\n")
+            last_price = "MEX = " + price + "$"
+            print("Input your face reaction in 2 seconds!\n")
             take_photo(camera)
         elif command == '3':
-            print("RIDE Value: " + str(get_ride_price())[:8] + "$\n\n")
+            price = str(get_ride_price())[:8]
+            print("RIDE Value: " + price + "$\n\n")
+            last_price = "RIDE = " + price + "$"
+            print("Input your face reaction in 2 seconds!\n")
             take_photo(camera)
         elif command == '4':
-            print("ITHEUM Value: " + str(get_itheum_price())[:8] + "$\n\n")
+            price = str(get_itheum_price())[:8]
+            print("ITHEUM Value: " + price + "$\n\n")
+            last_price = "ITHEUM = " + price + "$"
+            print("Input your face reaction in 2 seconds!\n")
             take_photo(camera)
+        elif command == '5':
+            show_reaction(last_price)
 
 
 main()
---
+++ main_win.py
@@ -0,0 +1,60 @@
+# crypto display
+import requests
+
+
+# Get eGLD Price
+def get_egld_price():
+    egld_info = requests.get('https://api.elrond.com/tokens/WEGLD-bd4d79').json()
+    egld_price = egld_info["price"]
+    return egld_price
+
+
+# Get MEX Price
+def get_mex_price():
+    mex_info = requests.get('https://api.elrond.com/tokens/MEX-455c57').json()
+    mex_price = mex_info["price"]
+    return mex_price
+
+
+# Get RIDE Price
+def get_ride_price():
+    ride_info = requests.get('https://api.elrond.com/tokens/RIDE-7d18e9').json()
+    ride_price = ride_info["price"]
+    return ride_price
+
+
+# Get ITHEUM Price
+def get_itheum_price():
+    itheum_info = requests.get('https://api.elrond.com/tokens/ITHEUM-df6f26').json()
+    itheum_price = itheum_info["price"]
+    return itheum_price
+
+
+def display_menu():
+    print("Type '1' to show eGLD price")
+    print("Type '2' to show MEX price")
+    print("Type '3' to show RIDE price")
+    print("Type '4' to show ITHEUM price")
+    print()
+    print("Type '0' to exit")
+    print()
+
+
+def main():
+    while True:
+        display_menu()
+        command = input(">>>")
+
+        if command == '0':
+            exit()
+        elif command == '1':
+            print("eGLD Value: " + str(get_egld_price())[:8] + "$\n\n")
+        elif command == '2':
+            print("MEX Value: " + str(get_mex_price())[:8] + "$\n\n")
+        elif command == '3':
+            print("RIDE Value: " + str(get_ride_price())[:8] + "$\n\n")
+        elif command == '4':
+            print("ITHEUM Value: " + str(get_itheum_price())[:8] + "$\n\n")
+
+
+main()
